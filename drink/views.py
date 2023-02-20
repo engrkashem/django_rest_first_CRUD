945f2c827aff73640285856e98ed095d2dfabe45
engrkashem <kashemaust@gmail.com>
2023-02-20 17:31:25 +0600

    first commit

---
+++ drink/views.py
@@ -0,0 +1,50 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+
+def drink_list(request, format=None):
+
+    if request.method=='GET':
+        # get all the drinks
+        drinks = Drink.objects.all()
+        # serialize them
+        serializer = DrinkSerializer(drinks, many=True)
+        # return json
+        # if we want to send other than object( python dictionery) then neen do add ** safe=false ** inside JesonResponse as parameter
+        return JsonResponse({'drinks':serializer.data})
+    
+    if request.method=='POST':
+        # take jeson data and convert
+        serializer=DrinkSerializer(data=request.data)
+        # check if data is valid
+        if serializer.is_valid():
+            # save the data to data base
+            serializer.save()
+            # send response
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT', 'DELETE'])
+def drink_detail(request, id, format= None):
+
+    try:
+        drink=Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method=='GET':
+        serializer=DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method=='PUT':
+        serializer=DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
